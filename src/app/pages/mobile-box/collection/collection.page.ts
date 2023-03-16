b57aad03cb3b985e0946659d20036eb796c30ead
petereneric <ericschumacher@outlook.de>
2023-03-16 11:30:31 +0100

    Page Sammlung, Statistik

--- src/app/pages/mobile-box/collection/collection.page.ts
+++ src/app/pages/mobile-box/collection/collection.page.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {DataService} from "../../../services/data/data.service";
+import {ConnApiService} from "../../../services/conn-api/conn-api.service";
+import {HttpResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-collection',
@@ -8,9 +10,22 @@ import {DataService} from "../../../services/data/data.service";
 })
 export class CollectionPage implements OnInit {
 
-  constructor(private dataService: DataService) { }
+  // urls
+  urlStatisticsMonth = "collection/statistics/month"
+
+  // data
+  lData = []
+
+  constructor(private changeDetecRef: ChangeDetectorRef, private connApi: ConnApiService, private dataService: DataService) { }
 
   ngOnInit() {
+    console.log("hier bin ich")
+    this.connApi.get(this.urlStatisticsMonth).subscribe((response: HttpResponse<any>) => {
+      this.lData = response.body
+      console.log("loaded from server")
+      console.log(this.lData)
+    })
+    console.log("after method")
   }
 
   onLink(link) {
@@ -20,4 +35,23 @@ export class CollectionPage implements OnInit {
   onScroll(event) {
     this.dataService.scroll(event.detail.scrollTop)
   };
+
+  transGramm(nGramm) {
+    let weight = {
+      value: nGramm,
+      unit: 'g'
+    }
+    if (weight.value / 1000 >= 1) {
+      weight.value = nGramm / 1000
+      weight.unit = 'kg'
+      if (weight.value / 1000 >= 1) {
+        weight.value = weight.value / 1000
+        weight.unit = 'Tonnen'
+      }
+    }
+
+    weight.value = parseFloat(weight.value).toFixed(0).replace('.', ',');
+    return weight;
+
+  }
 }
